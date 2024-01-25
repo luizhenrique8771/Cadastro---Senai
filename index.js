17f8ebe1810956c9ef677a72294097fbc4c54317
luizhenrique8771 <148584361+luizhenrique8771@users.noreply.github.com>
2024-01-25 09:21:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const sexualidade = prompt("\nQual sua opção sexual?\n \n1. Heterossexual \n2. Homossexual \n3. Gagossexual ")
+
+switch(sexualidade) {
+    case "1": 
+    alert("Você gosta de pessoas do sexo oposto.")
+    break
+
+    case "2": 
+    alert("Você gosta de pessoas do mesmo sexo.")
+    break
+
+    case "3":
+        alert("Você gosta de pessoas que gagagaguejam.")
+        break
+
+        default:
+            alert("Opçao inválida.")
+}
